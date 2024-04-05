a9a0d79a03eb9f434075d8b95a247c2aaedc0541
mypymypy <danich282@gmail.com>
2024-04-05 19:40:04 +0300

    refactor: change baseName

--- src/pages/Home.tsx
+++
@@ -1,9 +0,0 @@
-import { Weather } from '@/components/weather/Weather';
-
-export const HomePage: React.FC = () => {
-  return (
-    <main>
-      <Weather />
-    </main>
-  );
-};
--- src/store/store.ts
+++
@@ -1,6 +0,0 @@
-import { createContext, useContext } from 'react';
-import WeatherStore from './weatherStore/WeatherStore';
-
-const WeatherStoreContext = createContext(new WeatherStore());
-
-export const useWeatherStore = () => useContext(WeatherStoreContext);
